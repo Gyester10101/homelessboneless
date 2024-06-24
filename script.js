65f05d9ad19db024bdccf7d13dc914447415f07c
wwilliamwai <wongwilliam102@gmail.com>
2024-06-24 03:06:14 -0700

    implement 2nd nav for mobile

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const navButton2 = document.getElementById('nav2-button');
+const navBar2 = document.getElementById('nav-bar2');
+
+function toggleNav() {
+    if (navBar2.style.display === 'grid') {
+        navBar2.style.display = 'none';
+        navButton2.style.backgroundImage = 'url(./menu-arrow-down.png)';
+    } else {
+        navBar2.style.display = 'grid';
+        navButton2.style.backgroundImage = 'url(./menu-arrow-up.png)';
+    }
+}
+
+function checkWindowSize() {
+    if (window.innerWidth > 1260) {
+        navBar2.style.display = 'none';
+        navButton2.style.backgroundImage = 'url(./menu-arrow-down.png)';
+    }
+}
+navButton2.addEventListener("click", toggleNav);
+window.addEventListener('resize', checkWindowSize);
+
+checkWindowSize();
 const quizBackground = document.getElementById('transparent-background')
 const startQuizButton = document.getElementById('take-quiz')
 let quizNumber = 1;
