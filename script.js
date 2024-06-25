d1874af9bbad264018bc256e0beb9f2b19b8e18a
wwilliamwai <wongwilliam102@gmail.com>
2024-06-24 17:18:23 -0700

    words become smaller in 2ndheader

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const navButton2 = document.getElementById('nav2-button');
 const navBar2 = document.getElementById('nav-bar2');
+const navBar2String = navBar2.innerHTML;
 
 function toggleNav() {
     if (navBar2.style.display === 'grid') {
@@ -16,6 +17,15 @@ function checkWindowSize() {
         navBar2.style.display = 'none';
         navButton2.style.backgroundImage = 'url(./menu-arrow-down.png)';
     }
+    if (window.innerWidth <= 550) {
+        navBar2.innerHTML = ""
+        navBar2.innerHTML += `
+        <h2 class="nav-bar-text2"><a class="nav-option" href="./jobs.html">Jobs</a></h2>
+        <h2 class="nav-bar-text2"><a class="nav-option" href="https://fhs.fuhsd.org/">Cheap Housing</a></h2>
+        <h2 class="nav-bar-text2"><a class="nav-option" href="https://fhs.fuhsd.org/">Training</a></h2>
+        <h2 class="nav-bar-text2"><a class="nav-option" href="https://fhs.fuhsd.org/">Support</a></h2>
+        `
+    } else navBar2.innerHTML = navBar2String;
 }
 navButton2.addEventListener("click", toggleNav);
 window.addEventListener('resize', checkWindowSize);
